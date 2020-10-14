35f50d4b2dc8ea9278f994257cda659bde2d395d
Evie <Evie_Stathatos@baylor.edu>
2020-10-14 15:00:32 -0500

    commit Creating a Button Class

--- game_stats.py
+++ game_stats.py
@@ -7,8 +7,8 @@ class GameStats:
         # Call reset_stats() method so that stats are set properly 
         self.reset_stats()
 
-        # Start Alien Invasion in an active state.
-        self.game_active = True
+        # Start Alien Invasion in an inactive state.
+        self.game_active = False
 
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
